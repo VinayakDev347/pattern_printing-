14b68a61a84e1e13d3f20cca4a16723dff7be67a
vinayak-dev <vinayakdev347@gmail.com>
2025-03-01 23:34:27 +0530

    first commit

---
+++ patterns.py
@@ -0,0 +1,17 @@
+# buy a number 
+# n = int(input("Enter the rows: "))
+#-----------------------------1--------------------------------------
+"""
+*
+**
+***
+****
+*****
+
+# code
+for i in range(n):
+    for j in range(i+1):
+        print("*",end="")
+    print("")
+
+"""
