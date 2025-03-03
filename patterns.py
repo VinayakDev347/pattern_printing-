1891f9cfce1fa4a84ee341439288230be4056cf3
vinayak-dev <vinayakdev347@gmail.com>
2025-03-03 13:31:44 +0530

    added 6th pattern

--- patterns.py
+++ patterns.py
@@ -75,4 +75,28 @@ for i in range(n):
     for j in range(2 * i + 1):
         print("*",end=" ")
     print("")
+
+#another method
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(1,2*i):
+        print("*",end="")
+    print("")
 """
+#-----------------------------6--------------------------------------
+"""
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     *
+"""
+n = 5
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    print("")
