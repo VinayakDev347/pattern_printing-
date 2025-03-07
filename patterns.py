c2e5bd6b4a03a25ab7ff49b459d7fc03a6bc463a
vinayak-dev <vinayakdev347@gmail.com>
2025-03-07 23:18:44 +0530

    added 11th patternt

--- patterns.py
+++ patterns.py
@@ -178,3 +178,29 @@ for i in range (n,0,-1):
         print("*", end="")
     print("")
 """
+#-----------------------------11--------------------------------------
+"""
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+
+n = 5
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    for j in range(2 * i + 1):
+        print("*", end=" ")
+    print()
+for i in range(n - 2, -1, -1):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    for j in range(2 * i + 1):
+        print("*", end=" ")
+    print()"
+"""
