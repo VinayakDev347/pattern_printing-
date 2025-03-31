6625e60f591be234f9c3c3b635eab51e51969cdf
vinayak-dev <vinayakdev347@gmail.com>
2025-03-31 23:25:34 +0530

    added 21pattern

--- patterns.py
+++ patterns.py
@@ -342,3 +342,20 @@ for i in range(1, n+1):
         print(i*j, end=" ")
     print("")
 """
+#-----------------------------20--------------------------------------
+"""
+1 
+1 2 1 
+1 2 3 2 1 
+1 2 3 4 3 2 1 
+1 2 3 4 5 4 3 2 1
+
+n = 5
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    for k in range(i-1, 0, -1):
+        print(k, end=" ")
+        
+    print("")"
+"""
