e81dd6985b79cb46c7b4b79196304fe2cf779a60
vinayak-dev <vinayakdev347@gmail.com>
2025-09-04 18:01:25 +0530

    Added 24 pattern

--- patterns.py
+++ patterns.py
@@ -401,3 +401,24 @@ for i in range(1, rows + 1):
     print()
 
 """
+
+#-----------------------------24--------------------------------------
+"""
+A  
+B C  
+D E F  
+G H I J  
+K L M N O  
+P Q R S T U  
+V W X Y Z [ \ 
+
+ascii_number = 65
+rows = 7
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        character = chr(ascii_number)
+        print(character, end=' ')
+        ascii_number += 1
+    print(" ")
+
+"""
