a92d232cfb89537b706d8986c2c9f24c5a121187
vinayak-dev <vinayakdev347@gmail.com>
2025-09-06 17:19:57 +0530

    Added 26 pattern

--- patterns.py
+++ patterns.py
@@ -440,3 +440,23 @@ for i in range(1, rows):
         print(format(2 ** j, "4d"), end=' ')
     print("")
 """
+#-----------------------------26--------------------------------------
+"""
+1 * 2 * 3 * 4 
+
+1 * 2 * 3 
+
+1 * 2 
+
+1
+
+row = 4
+for i in range(0, row):
+    c = 1
+    print(c, end=' ')
+    for j in range(row - i - 1, 0, -1):
+        print('*', end=' ')
+        c = c + 1
+        print(c, end=' ')
+    print('\n')
+"""
